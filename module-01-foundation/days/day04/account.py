27b9af71863eaa2a7a825d2675ab02520fecfd7e
hermi <hermelaashagre5@gmail.com>
2026-07-18 23:43:17 +0300

    Add large project day04

---
+++ module-01-foundation/days/day04/account.py
@@ -0,0 +1,26 @@
+class Account:
+    def __init__(self, owner, number, balance=0):
+        self.owner = owner
+        self.account_number = number
+        self.__balance = balance
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Amount must be positive")
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError("Amount must be positive")
+        if amount > self.__balance:
+            raise ValueError("Insufficient balance")
+        self.__balance -= amount
+
+    def statement(self):
+        print("Owner:", self.owner)
+        print("Account Number:", self.account_number)
+        print("Balance: ETB", self.__balance)
---
+++ module-01-foundation/days/day04/practice.py
@@ -0,0 +1,8 @@
+from account import Account
+
+account = Account("Hermela Ashagre", "ACC1001", 1000)
+
+account.deposit(500)
+account.withdraw(300)
+
+account.statement()

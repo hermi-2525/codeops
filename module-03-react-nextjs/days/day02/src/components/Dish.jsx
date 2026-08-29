5600f610f5a4cfc76d626fa87fdcf1b4bd5f6b10
hermi <hermelaashagre5@gmail.com>
2026-08-30 01:36:08 +0300

    added module 3 day02 exercise

---
+++ module-03-react-nextjs/days/day02/src/components/Dish.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+function Dish({
+  name,
+  price,
+  currency = "ETB",
+  spicy = false
+}) {
+  return (
+    <div className="dish">
+      <h3>
+        {name} {spicy && <span>• Spicy</span>}
+      </h3>
+
+      <p>
+        {price} {currency}
+      </p>
+    </div>
+  );
+}
+
+Dish.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  spicy: PropTypes.bool,
+};
+
+export default Dish;
---
+++ module-03-react-nextjs/days/day02/src/data.js
@@ -0,0 +1,53 @@
+const menuItems = [
+  {
+    id: 1,
+    name: "Doro Wat",
+    price: 240,
+    category: "Main",
+    spicy: true,
+  },
+  {
+    id: 2,
+    name: "Tibs",
+    price: 280,
+    category: "Main",
+    spicy: true,
+  },
+  {
+    id: 3,
+    name: "Shiro",
+    price: 180,
+    category: "Vegetarian",
+    spicy: false,
+  },
+  {
+    id: 4,
+    name: "Kitfo",
+    price: 350,
+    category: "Main",
+    spicy: false,
+  },
+  {
+    id: 5,
+    name: "Fasting Firfir",
+    price: 160,
+    category: "Vegetarian",
+    spicy: true,
+  },
+  {
+    id: 6,
+    name: "Gomen",
+    price: 150,
+    category: "Vegetarian",
+    spicy: false,
+  },  
+  {
+    id: 7,
+    name: "Chechebsa",
+    price: 200,
+    category: "Breakfast",
+    spicy: false,
+  }
+];
+
+export default menuItems;
